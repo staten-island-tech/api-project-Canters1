7654027a162facd46235cd2613a7d2031efeb286
Yan Sharma <yawning008@gmail.com>
2023-11-17 10:58:30 -0500

    Done

--- api/js/api.js
+++ api/js/api.js
@@ -22,20 +22,63 @@ function today(x){
 }
 }
 
-function filter(year,month,day, x){
+function filter(year, month, day, x) {
+
+  if (year === "" || year<1) {
+    alert("please enter a  positivevalue for year");
+    return
+  } 
+  else if(month <1 || month>12){
+    alert("please enter a value for month between 1 and 12")
+    return
+  }
+  else if(day<1 || day>31){
+    alert("please enter a day between 1 and 31")
+    return
+  }
+  else if (day === "" && month !== "") {
+    
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://calendarific.com/api/v2/holidays?&api_key=FwtJjJUkjlmYLzrga2IHDnlcafMzUffk&country=US&year=${year}&month=${month}&day=${day}`);
-    xhr.responseType = 'json'; 
+    xhr.open('GET', `https://calendarific.com/api/v2/holidays?&api_key=FwtJjJUkjlmYLzrga2IHDnlcafMzUffk&country=US&year=${year}&month=${month}`);
+    xhr.responseType = 'json';
     xhr.send();
 
     xhr.onload = function() {
       const response = xhr.response.response;
-      const holidays=response.holidays
-      console.log(holidays)
-      x(holidays)
+      const holidays = response.holidays;
+      console.log(holidays);
+      x(holidays);
     }
-}
 
+  } else if (day === "" && month === "") {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://calendarific.com/api/v2/holidays?&api_key=FwtJjJUkjlmYLzrga2IHDnlcafMzUffk&country=US&year=${year}`); 
+    xhr.responseType = 'json';
+    xhr.send();
+
+    xhr.onload = function() {
+      const response = xhr.response.response;
+      const holidays = response.holidays;
+      console.log(holidays);
+      x(holidays);
+    }
+
+  } else {
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://calendarific.com/api/v2/holidays?&api_key=FwtJjJUkjlmYLzrga2IHDnlcafMzUffk&country=US&year=${year}&month=${month}&day=${day}`);
+    xhr.responseType = 'json';
+    xhr.send();
+
+    xhr.onload = function() {
+      const response = xhr.response.response;
+      const holidays = response.holidays;
+      console.log(holidays);
+      x(holidays);
+    }
+  }
+}
 
 export {filter}
 export {today}
--- api/js/main.js
+++ api/js/main.js
@@ -8,32 +8,36 @@ const domselectors = {
   dayinput : document.querySelector("#day"),
 }
 
-function cards(x){
-  x.forEach((holiday)=> domselectors.wrapper.insertAdjacentHTML(
-      "afterbegin",
-      `<div class = "card">
-          <div class="card-content">
-              <h2>
-                  ${holiday.name}
-              </h2>
-              <p>
-                  ${holiday.description}
-              </p>
-
-          </div>
-      </div>`
-    ));
-    console.log("it ran")
-    }
-
-today(cards)
+function cards(x) {
+    const rendered = []
+    x.forEach((holiday) => {
+      if (!rendered.includes(holiday.name)) {
+        rendered.push(holiday.name);
+        domselectors.wrapper.insertAdjacentHTML(
+          "afterbegin", 
+          `<div class="card">
+            <div class="card-content">
+              <h2>${holiday.name}</h2> 
+              <p>${holiday.description}</p>
+            </div>
+          </div>`
+        ); 
+      }
+    });
+  
+    console.log("it ran");
+  }
 
 
 domselectors.form.addEventListener('submit', function(event) { 
     event.preventDefault();
+    while (domselectors.wrapper.firstChild) {
+        domselectors.wrapper.removeChild(domselectors.wrapper.lastChild);
+      }
     const year = domselectors.yearinput.value
     const month = domselectors.monthinput.value
     const day = domselectors.dayinput.value
     filter(year, month, day, cards)
 }
     );
+document.addEventListener('DOMContentLoaded', today(cards))
