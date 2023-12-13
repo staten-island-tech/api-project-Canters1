a7fe4a9acb9b89a0e95cf4a562078ca12ad8defe
Yan Sharma <yawning008@gmail.com>
2023-12-13 10:59:48 -0500

    Update api.js

--- api/js/api.js
+++ api/js/api.js
@@ -44,7 +44,7 @@ async function today(x) {
 
       const data = await response.json();
       const holidays = data.response.holidays; 
-      nocards(holidays);
+      nocards(data.response);
       x(holidays);
 
     } catch(err) {
